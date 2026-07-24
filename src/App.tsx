654eaa0e6cd4cebcee86ccbd7eb517aa5522a821
brightskyconstruction-prog <brightskyconstruction@gmail.com>
2026-07-24 20:49:42 +0530

    history tab update

--- src/App.tsx
+++ src/App.tsx
@@ -573,7 +573,7 @@ function AppInner() {
                 ...styles.tab,
                 flex: tab.flex,
                 ...(isActive ? styles.tabActive : {}),
-                fontSize: lang === 'en' ? '15px' : '12.5px',
+                fontSize: lang === 'en' ? '15px' : '12px',
               }}
               onClick={() => handleTabSwitch(tab.id)}
             >
